85379f4b2615ebbde2abf2087c772600d89e346b
damianavidal <126358080+damianavidal@users.noreply.github.com>
2023-02-27 20:03:18 -0300

    Add files via upload

---
+++ js/funcion.js
@@ -0,0 +1,80 @@
+function FunctionInfo() {
+    var x = document.getElementById("info");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function FunctionDir() {
+    var x = document.getElementById("direccion");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function FunctionEmail() {
+    var x = document.getElementById("email");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function FunctionTel() {
+    var x = document.getElementById("Telefono");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function FunctionLin() {
+    var x = document.getElementById("link");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function FunctionEstu() {
+    var x = document.getElementById("estudios");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function FunctionExp() {
+    var x = document.getElementById("exp");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }  
+
+  function FunctionHab() {
+    var x = document.getElementById("hab");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function FunctionCont() {
+    var x = document.getElementById("cont");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
